27ee6ba7233ebd178b56a301d90f2cc1c8cea2c3
David Tuttle <dtuttle@umass.edu>
2015-08-22 08:35:35 -0400

    Coded a deletion mechanism

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -13,3 +13,46 @@ public class PlayerInput : MonoBehaviour {
 
 	}
 }
+/**
+ * 
+using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour 
+{
+	private PlayerController playerController;
+	private KeyCode launchKey = KeyCode.Space;
+	private KeyCode cameraResetKey = KeyCode.R;
+	private KeyCode gameResetKey = KeyCode.F;
+	private OVRDisplay cameraDisplay;
+
+	// Use this for initialization
+	void Start () 
+	{
+		playerController = GetComponent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(!playerController.HasLaunched)
+		{
+			if(Input.GetKeyDown(cameraResetKey)) // reset camera
+			{
+				cameraDisplay.RecenterPose();
+			}
+			else if (Input.GetKeyDown(launchKey)) // try launching
+			{
+				playerController.TryLaunch();
+			}
+		}
+		else
+		{
+			if(Input.GetKeyDown(gameResetKey))
+			{
+
+			}
+		}
+	}
+}
+ **/
